07a0906e38d99a6a86a46538afae1b3e64ab5386
Wesley Gimenes <wehagy@proton.me>
2025-07-23 02:10:00 -0300

    scripts/build.sh: fix indentation
    Signed-off-by: Wesley Gimenes <wehagy@proton.me>

--- scripts/build.sh
+++ scripts/build.sh
@@ -110,91 +110,91 @@ rebuild_translations() {
 }
 
 generate_icons() {
-    show_log "INFO" "Checking required dependencies..."
+  show_log "INFO" "Checking required dependencies..."
 
-    MISSING_DEPENDENCY=false
+  MISSING_DEPENDENCY=false
 
-    if ! command -v optipng >/dev/null 2>&1 ; then
-        MISSING_DEPENDENCY=true
-        show_log "ERROR" "Missing optipng dependency."
-    fi
+  if ! command -v optipng >/dev/null 2>&1 ; then
+    MISSING_DEPENDENCY=true
+    show_log "ERROR" "Missing optipng dependency."
+  fi
 
-    if ! command -v inkscape >/dev/null 2>&1 ; then
-        MISSING_DEPENDENCY=true
-        show_log "ERROR" "Missing inkscape dependency."
-    fi
-    
-    if $MISSING_DEPENDENCY = true ; then
-        show_log "ERROR" "Dependency check failed."
-        exit 1;
-    fi
+  if ! command -v inkscape >/dev/null 2>&1 ; then
+    MISSING_DEPENDENCY=true
+    show_log "ERROR" "Missing inkscape dependency."
+  fi
+  
+  if $MISSING_DEPENDENCY = true ; then
+    show_log "ERROR" "Dependency check failed."
+    exit 1;
+  fi
 
-    show_log "PASS" "Dependency check successful."
+  show_log "PASS" "Dependency check successful."
 
-    show_log "INFO" "Generating icons..."
+  show_log "INFO" "Generating icons..."
 
-    SVG_DIR="data/logo"
-    EXPORT_DIR="data/logo/icons/hicolor"
-    ICON_SIZES=(512 256 128 64 48 32 16)
+  SVG_DIR="data/logo"
+  EXPORT_DIR="data/logo/icons/hicolor"
+  ICON_SIZES=(512 256 128 64 48 32 16)
 
-    for svg_file in "${SVG_DIR}"/*.svg; do
-        svg_name="$(basename "${svg_file}")"
+  for svg_file in "${SVG_DIR}"/*.svg; do
+    svg_name="$(basename "${svg_file}")"
 
-        for size in "${ICON_SIZES[@]}"; do
-            png_output_dir="${EXPORT_DIR}/${size}x${size}/apps"
-            if [[ ! -d "${png_output_dir}" ]]; then
-                mkdir -p "${png_output_dir}"
-            fi
+    for size in "${ICON_SIZES[@]}"; do
+      png_output_dir="${EXPORT_DIR}/${size}x${size}/apps"
+      if [[ ! -d "${png_output_dir}" ]]; then
+        mkdir -p "${png_output_dir}"
+      fi
 
-            png_file="${png_output_dir}/${svg_name%.*}.png"
+      png_file="${png_output_dir}/${svg_name%.*}.png"
 
-            inkscape --export-type="png" \
-                --export-filename="${png_file}" \
-                --export-area-page \
-                --export-width="${size}" \
-                --export-height="${size}" \
-                "${svg_file}"
+      inkscape --export-type="png" \
+        --export-filename="${png_file}" \
+        --export-area-page \
+        --export-width="${size}" \
+        --export-height="${size}" \
+        "${svg_file}"
 
-            optipng -o7 "${png_file}"
-        done
+      optipng -o7 "${png_file}"
     done
+  done
 
-    show_log "PASS" "Icons successfully generated."
+  show_log "PASS" "Icons successfully generated."
 }
 
 flathub_linter() {
-    show_log "INFO" "Linting the local source code..."
+  show_log "INFO" "Linting the local source code..."
 
-    flatpak_dependency_check
-    # We must perform a Flatpak build *and* export to a ostree "repo" directory.
-    # NOTE: We will perform a LOCAL build so that we check the LOCAL manifest.
-    # NOTE: We don't trigger INSTALL in this case, since we're just linting.
-    BUILD_VARIANT="local"
-    BUILD_MANIFEST="com.vysp3r.ProtonPlus.local.yml"
-    BUILD_DIR="build-flatpak/${BUILD_VARIANT}/build"
-    BUILD_OSTREE_REPO="build-flatpak/${BUILD_VARIANT}/repo"
-    flatpak run org.flatpak.Builder --verbose \
+  flatpak_dependency_check
+  # We must perform a Flatpak build *and* export to a ostree "repo" directory.
+  # NOTE: We will perform a LOCAL build so that we check the LOCAL manifest.
+  # NOTE: We don't trigger INSTALL in this case, since we're just linting.
+  BUILD_VARIANT="local"
+  BUILD_MANIFEST="com.vysp3r.ProtonPlus.local.yml"
+  BUILD_DIR="build-flatpak/${BUILD_VARIANT}/build"
+  BUILD_OSTREE_REPO="build-flatpak/${BUILD_VARIANT}/repo"
+  flatpak run org.flatpak.Builder --verbose \
     --sandbox --force-clean --ccache \
     --repo="${BUILD_OSTREE_REPO}" \
     "${BUILD_DIR}" \
     "${BUILD_MANIFEST}"
 
-    # Allow command failures after this point, since linters may exit with errors.
-    set +e
+  # Allow command failures after this point, since linters may exit with errors.
+  set +e
 
-    # Now we can run the Flathub linters.
-    flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest "${BUILD_MANIFEST}"
-    flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream "${BUILD_DIR}/export/share/metainfo/com.vysp3r.ProtonPlus.metainfo.xml"
-    flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo "${BUILD_OSTREE_REPO}"
+  # Now we can run the Flathub linters.
+  flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest "${BUILD_MANIFEST}"
+  flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream "${BUILD_DIR}/export/share/metainfo/com.vysp3r.ProtonPlus.metainfo.xml"
+  flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo "${BUILD_OSTREE_REPO}"
 
-    set +x
-    show_log "INFO" "The following errors can be safely ignored."
-    show_log "INFO" "\"appstream-screenshots-not-mirrored-in-ostree\" (only happen in local build, but will not happen on Flathub)"
-    show_log "INFO" "\"appstream-external-screenshot-url\" (only happen in local build, but will not happen on Flathub)"
-    show_log "INFO" "\"finish-args-flatpak-appdata-folder-access\" (we need to access the host filesystem since Steam libraries can be anywhere)"
-    show_log "INFO" "\"finish-args-flatpak-spawn-access\" (necessary to be able to manage STL)"
-    show_log "INFO" "\"appid-filename-mismatch: com.vysp3r.ProtonPlus.local\" (only happens on the local source code)"
-    show_log "INFO" "Done linting the local source code..."
+  set +x
+  show_log "INFO" "The following errors can be safely ignored."
+  show_log "INFO" "\"appstream-screenshots-not-mirrored-in-ostree\" (only happen in local build, but will not happen on Flathub)"
+  show_log "INFO" "\"appstream-external-screenshot-url\" (only happen in local build, but will not happen on Flathub)"
+  show_log "INFO" "\"finish-args-flatpak-appdata-folder-access\" (we need to access the host filesystem since Steam libraries can be anywhere)"
+  show_log "INFO" "\"finish-args-flatpak-spawn-access\" (necessary to be able to manage STL)"
+  show_log "INFO" "\"appid-filename-mismatch: com.vysp3r.ProtonPlus.local\" (only happens on the local source code)"
+  show_log "INFO" "Done linting the local source code..."
 }
 
 main() {
